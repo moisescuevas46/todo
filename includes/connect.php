2240188740f15672645a189ca6cc233053fb2edf
Moises A. Cuevas <moisescuevas46@gmail.com>
2015-04-15 12:19:39 -0700

    made an include file and put connect.php in there and made some php error into errno

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php 
-	$mysqli = new mysqli('localhost','root','root','tasks');
+	$mysqli = new mysqli('localhost','root','root','todo');
 	if($mysqli -> connect_error){
-		die ('Connect Error (' . $myqli->connect_error . ')')
+		die ('Connect Error (' . $mysqli->connect_errno . ')'
 		. $mysqli->connect_error);
 	}
 	else{
