9cbfe1ea46bd8fe38d2ec718a8a506ae2398ddd0
Moises A. Cuevas <moisescuevas46@gmail.com>
2015-04-16 17:51:37 -0700

    video 16 done, got it so that it echos out the button

--- index.php
+++ index.php
@@ -17,12 +17,13 @@
 					$task_id = $row['id'];
 					$task_name = $row['task'];
 
-					echo "<li><span>'.$task_name'";
+					echo '<li>
+					<span>'.$task_name. '</span>
+					<img id = "'.$task_id.'"class = "delete-button" width="10px" src="images/close.svg"/>
+					</li>';
 				}
 			}
 		}
-
-
 		?>	
 		</ul>
 	</div>
